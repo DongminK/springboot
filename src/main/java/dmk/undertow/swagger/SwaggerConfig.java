88fd5560d5952329511172347b8f9b6c6caeaccb
Dongmin <kdm0228@gmail.com>
2020-09-21 17:02:56 +0900

    swagger 인증 버튼 추가

--- src/main/java/dmk/undertow/swagger/SwaggerConfig.java
+++ src/main/java/dmk/undertow/swagger/SwaggerConfig.java
@@ -1,12 +1,18 @@
 package dmk.undertow.swagger;
 
+import java.util.List;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import com.google.common.base.Predicates;
+import com.google.common.collect.Lists;
 
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiKey;
+import springfox.documentation.service.AuthorizationScope;
+import springfox.documentation.service.SecurityReference;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -24,7 +30,26 @@ public class SwaggerConfig {
 				.apis(Predicates.not(RequestHandlerSelectors.basePackage("org.springframework.boot")))
 				//.paths(PathSelectors.ant("/v1/api/**"))
 				.paths(PathSelectors.any())
-				.build();
+				.build()
+				.securityContexts(Lists.newArrayList(securityContext())) 
+				.securitySchemes(Lists.newArrayList(apiKey()));
 	}
+	
+	private ApiKey apiKey() {
+		return new ApiKey("X-Authtication", "X-Authtication", "header");
+	}
+
+	private springfox.documentation.spi.service.contexts.SecurityContext securityContext() {
+		return springfox.documentation.spi.service.contexts.SecurityContext.builder().securityReferences(defaultAuth())
+				.forPaths(PathSelectors.any()).build();
+	}
+
+	List<SecurityReference> defaultAuth() {
+		AuthorizationScope authorizationScope = new AuthorizationScope("global", "accessEverything");
+		AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
+		authorizationScopes[0] = authorizationScope;
+		return Lists.newArrayList(new SecurityReference("X-Authtication", authorizationScopes));
+	}
+
 
 }
